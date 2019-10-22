3e84111ac02c8430315bc5a263e27c7e60a8126f
Naya  Willis <greedybrain18@gmail.com>
2019-10-22 14:00:53 +0000

    first commit

---
+++ lib/website_worth.rb
@@ -0,0 +1,6 @@
+require "website_worth/version"
+
+module WebsiteWorth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/website_worth/version.rb
@@ -0,0 +1,3 @@
+module WebsiteWorth
+  VERSION = "0.1.0"
+end
