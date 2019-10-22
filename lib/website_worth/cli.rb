d8663a76ae2b02ef0e0b60a76265f101a7d78846
Naya  Willis <greedybrain18@gmail.com>
2019-10-22 19:54:58 +0000

    changed #peak_curiosity to #intro

--- lib/website_worth/cli.rb
+++ lib/website_worth/cli.rb
@@ -3,8 +3,10 @@ require 'pry'
 class WebsiteWorth::CLI 
 
   def call 
-    peak_curiosity
-    try_a_site
+    intro
+    get_user_site
+    # export_to_CSV < additional feature maybe
+    see_you_later
   end
   
   def big_name_site(site)
@@ -21,11 +23,14 @@ class WebsiteWorth::CLI
     big_name_data
   end
   
-  def peak_curiosity
+  def intro
     puts "Revenue and Traffic Data of Popular Websites\n\n========================================\n\nGoogle - Est. Worth > #{big_name_site('google.com')[0]} - Est. Visits/per day > #{big_name_site('google.com')[1]}\n\nAmazon - Est. Worth > #{big_name_site('amazon.com')[0]} - Est. Visits/per day > #{big_name_site('amazon.com')[1]}\n\nFacebook - Est. Worth > #{big_name_site('facebook.com')[0]} - Est. Visits/per day > #{big_name_site('facebook.com')[1]}"
   end
   
-  def try_a_site
+  def get_user_site
+    puts "\nChoose a Website. You can even choose one of the big names mentioned above to get a little more info on them!"
+    puts
+    users_site = gets.chomp.to_s
   end
 
 end
--- lib/website_worth/scraper.rb
+++ lib/website_worth/scraper.rb
@@ -1,14 +1,11 @@
 require 'pry'
 
 class WebsiteWorth::Scraper
-
-  <<-DOC
-    The responsibility of the Scraper class is to get the websites(worthofweb...) html to parse.
-    1. This is solely dependent on; if a user provides a valid website name
-    2. User can provide a website with any TLD extension eg. [.co, .net, .org] etc
-  DOC
   
+  attr_accessor :url
   
+  def scrape_site_based_on_user_choice
+    
+  end
   
-
 end
