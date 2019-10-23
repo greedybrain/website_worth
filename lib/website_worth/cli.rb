5ed80c2d110338ed448b8bfee46c28f4a5af8672
Naya  Willis <greedybrain18@gmail.com>
2019-10-23 11:43:38 +0000

    created #call

--- lib/website_worth/cli.rb
+++ lib/website_worth/cli.rb
@@ -3,35 +3,17 @@ require 'pry'
 class WebsiteWorth::CLI 
 
   def call 
-    intro
-    get_user_site
-    # export_to_CSV < additional feature maybe
-    see_you_later
-  end
-  
-  def big_name_site(site)
-    big_name_data = []
-  
-    website = Nokogiri::HTML(open("https://www.worthofweb.com/website-value/#{site}/"))
-  
-    worth = website.css('div.card-body div.side-left p:nth-of-type(2)')[0].text 
-    visits = website.css('div.card-body .col-md-4 p:nth-of-type(2)')[3].text
-  
-    big_name_data << worth 
-    big_name_data << visits
-  
-    big_name_data
+    intro # Hello (name), welcome to website worth checker. 
+    shows_ex_big_names # Shows user a list of big names and there worth 
+    get_site_info # prompts user to choose a site that they're interested in 
+    save_data_to_cpu? # MAYBE feature, prompts user to choose if they want to save data 
   end
   
   def intro
-    puts "Revenue and Traffic Data of Popular Websites\n\n========================================\n\nGoogle - Est. Worth > #{big_name_site('google.com')[0]} - Est. Visits/per day > #{big_name_site('google.com')[1]}\n\nAmazon - Est. Worth > #{big_name_site('amazon.com')[0]} - Est. Visits/per day > #{big_name_site('amazon.com')[1]}\n\nFacebook - Est. Worth > #{big_name_site('facebook.com')[0]} - Est. Visits/per day > #{big_name_site('facebook.com')[1]}"
+    # get name by accessing user class
+    puts "Hello #{}"
   end
   
-  def get_user_site
-    puts "\nChoose a Website. You can even choose one of the big names mentioned above to get a little more info on them!"
-    puts
-    users_site = gets.chomp.to_s
-  end
 
 end
 
