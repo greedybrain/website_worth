ae11719c1785e8473061d572b307e08fe85bde85
Caspa <51010236+greedybrain@users.noreply.github.com>
2019-10-29 15:11:25 -0400

    Update user.rb

--- lib/website_worth/user.rb
+++ lib/website_worth/user.rb
@@ -1,40 +1,28 @@
-require "pry"
-
-class WebsiteWorth::User 
+class WebsiteWorth::User
   
   attr_reader :name
 
   # responsible for providing his/her name and the necessary info to scrape
-  def initialize 
-    @name = gets.chomp
-    @name = @name[0].upcase + @name[1..-1].downcase
+  def initialize
+    begin
+      @name = gets.chomp.to_s
+      @name = @name[0].upcase + @name[1..-1].downcase
+    rescue 
+      @name = "User"
+    end
   end
   
   def self.gives_a_site_name
     while true
-      print "Enter the website that you're interested in > (eg. google.com, repl.it, yoursite.net): "
       user_choice = gets.chomp.downcase.to_s
-      if user_choice != ("" || nil)
+      if user_choice == ""
+        print "\nPlease enter a valid website name: "
+        next
+      else
         break
-      else
-        puts "Please enter a valid website name"
-        puts
       end
     end
-    user_choice[0].upcase + user_choice[1..-1].downcase
-    # binding.pry
-  end
-  
-  def self.search_another_site?
-    while true
-      puts "Would you like to search another site?"
-      user_answer = gets.chomp.downcase.to_s 
-      if user_choice != ("" || nil)
-        User.gives_a_site_name
-      else
-        puts "Please type yes or no."
-      end
-    end
+    user_choice
   end
 
 end
